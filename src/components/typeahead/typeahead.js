3fc16799329c4835a2de5dfc7d9550592ec941af
Hélder Pereira <blackspiider.86@gmail.com>
2017-10-27 01:41:05 +0100

    show profile and some improves
    - select a option using only keyboard fixed
    - select a option open a profile
    - fixed dropw showing when don't have options

--- src/components/typeahead/typeahead.js
+++ src/components/typeahead/typeahead.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import onClickOutside from 'react-onclickoutside';
+import Profile from '../profile';
 import 'whatwg-fetch';
 
 const StyledWrapper = styled.div`
@@ -17,10 +18,12 @@ const SuggestedList = styled.ul`
 const SuggestionHighlight = styled.li`
   background: lightgrey;
   padding: 10px;
+  cursor: pointer;
 `
 
 const SuggestionLine = styled.li`
   padding: 10px;
+  cursor: pointer;
 `
 
 const SuggestionsList = styled.div`
@@ -64,6 +67,8 @@ class Typeahead extends Component {
           openList: false,
           highLighted: -1,
           fetchData: props.fetchData,
+          showProfile: false,
+          user: null,
       };
   }
 
@@ -84,20 +89,35 @@ class Typeahead extends Component {
             { part }
         </span>)
     } </span>;
-}
+  }
 
+  handleSelectClick = (user) => {
+    this.setState({
+      user,
+      showProfile: true,
+      openList: false,
+    });
+  }
+
+  handlModelClose = () => {
+    this.setState({
+      showProfile: false,
+    });
+  }
 
   _handleKeys = (e) => {
     const { highLighted } = this.state;
+    const listSize = this.props.data.length;
+
     switch(e.keyCode) {
       case 40: // Down key
         this.setState({
-          highLighted: +highLighted + 1
+          highLighted: (highLighted + 1 >= listSize) ? 0 : highLighted + 1,
         });
         break;
       case 38: // Up key
         this.setState({
-          highLighted: +highLighted - 1
+          highLighted: (highLighted - 1 < 0) ? listSize-1 : highLighted - 1,
         });
         break;
       case 27: // ESC
@@ -105,32 +125,12 @@ class Typeahead extends Component {
           openList: false,
         });
         break;
+      case 13: // Enter
+        this.handleSelectClick(this.props.data[highLighted])
+        break;
       default:
         // do nothing
     }
-  }
-
-  render() {
-    const { getHighlightedText } = this;
-    const { searchValue } = this.state;
-    return (
-      <StyledWrapper>
-        <StyledInput
-            type='text'
-            value={this.state.searchValue}
-            onChange={this._handleSearchValueChange}
-            placeholder='Enter Member Name or Number...'
-            onKeyDown={this._handleKeys}
-            openList={this.state.openList}
-          />
-            <SuggestionsList openList={this.state.openList} >
-              <SuggestedList>
-                {this.props.data.map((ent, index) => index === this.state.highLighted ?
-                  <SuggestionHighlight>{getHighlightedText(ent.name, searchValue)}</SuggestionHighlight> : <SuggestionLine>{getHighlightedText(ent.name, searchValue)}</SuggestionLine>)}
-              </SuggestedList>
-            </SuggestionsList>
-      </StyledWrapper>
-    )
   }
 
   _handleSearchValueChange = e => {
@@ -142,11 +142,40 @@ class Typeahead extends Component {
         fetchData(e.target.value);
       }
 
-
       this.setState({
-          searchValue: value,
-          openList: true,
+        searchValue: value,
+        openList: true,
       });
+  }
+
+  render() {
+    const { getHighlightedText, openProfile } = this;
+    const { searchValue } = this.state;
+    return (
+      <div>
+        <StyledWrapper>
+          <StyledInput
+              type='text'
+              value={this.state.searchValue}
+              onChange={this._handleSearchValueChange}
+              placeholder='Enter Member Name or Number...'
+              onKeyDown={this._handleKeys}
+              openList={this.state.openList}
+            />
+              {/* prevent from show dropdown wihout data need improve */}
+              {this.state.searchValue.length > 0 && this.props.data.length > 0 &&
+                <SuggestionsList openList={this.state.openList} >
+                <SuggestedList>
+                  {this.props.data.map((ent, index) => index === this.state.highLighted ?
+                    <SuggestionHighlight key={ent.id} onClick={this.handleSelectClick.bind(this, ent)} >{getHighlightedText(ent.name, searchValue)}</SuggestionHighlight> :
+                    <SuggestionLine key={ent.id} onClick={this.handleSelectClick.bind(this,  ent)} >{getHighlightedText(ent.name, searchValue)}</SuggestionLine>)}
+                </SuggestedList>
+              </SuggestionsList>}
+        </StyledWrapper>
+        {this.state.showProfile && <Profile user={this.state.user} onClose={this.handlModelClose} />}
+      </div>
+
+    )
   }
 }
 
